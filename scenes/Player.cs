1e72fdac0d3a87c89d15d5f62ebd2391367b58e2
dfcut8 <dfcut8@proton.me>
2025-08-18 10:02:04 -0400

    basic movement

--- scenes/Player.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public partial class Player : CharacterBody2D
-{
-    public override void _PhysicsProcess(double delta)
-    {
-
-    }
-}
---
+++ scripts/Player.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    [Export]
+    public float speed = 300;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var moveVelocity = new Vector2();
+        if (Input.IsActionPressed("player_move_right"))
+        {
+            moveVelocity.X += speed;
+        }
+        if (Input.IsActionPressed("player_move_left"))
+        {
+            moveVelocity.X -= speed;
+        }
+        if (Input.IsActionPressed("player_move_down"))
+        {
+            moveVelocity.Y += speed;
+        }
+        if (Input.IsActionPressed("player_move_up"))
+        {
+            moveVelocity.Y -= speed;
+        }
+        Velocity = moveVelocity;
+        MoveAndSlide();
+    }
+}
