056f58eaeeab52393851b078b170474ec89cf6ed
dfcut8 <dfcut8@proton.me>
2025-08-22 10:28:53 -0400

    simple signal to free

--- scripts/Rocket.cs
+++ scripts/Rocket.cs
@@ -6,6 +6,11 @@ public partial class Rocket : Area2D
 {
     [Export]
     public float speed = 500f;
+    public override void _Ready()
+    {
+        var notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleNotifier");
+        notifier.ScreenExited += () => QueueFree();
+    }
     public override void _PhysicsProcess(double delta)
     {
         GlobalPosition = new Vector2(GlobalPosition.X + speed * (float)delta, GlobalPosition.Y);
