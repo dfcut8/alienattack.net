24f50b4fa9a00eef6e13a90d6d8c8e890742a5bc
dfcut8 <dfcut8@proton.me>
2025-08-22 11:54:55 -0400

    make it work

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -3,9 +3,9 @@ using Godot;
 public partial class Enemy : Area2D
 {
     [Export]
-    private float speed;
+    private float speed = 100;
     public override void _PhysicsProcess(double delta)
     {
-        GlobalPosition += new Vector2(GlobalPosition.X - speed * (float)delta, 0);
+        GlobalPosition = new Vector2(GlobalPosition.X - speed * (float)delta, GlobalPosition.Y);
     }
 }
