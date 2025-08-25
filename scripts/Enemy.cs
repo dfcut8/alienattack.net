d4b9cb8200f30def31ded64ba03b471a4566fd70
dfcut8 <dfcut8@proton.me>
2025-08-25 11:14:26 -0400

    add deathzone

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -4,6 +4,7 @@ public partial class Enemy : Area2D
 {
     [Export]
     private float speed = 100;
+
     public override void _PhysicsProcess(double delta)
     {
         GlobalPosition = new Vector2(GlobalPosition.X - speed * (float)delta, GlobalPosition.Y);
--- scripts/Game.cs
+++ scripts/Game.cs
@@ -2,6 +2,17 @@ using Godot;
 
 public partial class Game : Node2D
 {
+    [Export]
+    private Area2D deathZone;
+
+    public override void _Ready()
+    {
+        deathZone.AreaEntered += (Area2D a) =>
+        {
+            GD.Print($"Deathzone: Collided with: {a.Name}");
+            a.QueueFree();
+        };
+    }
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed("global_reset_level"))
