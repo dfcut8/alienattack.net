3e66efe5be5d470b08dcb7a12e059c7544156ee8
dfcut8 <dfcut8@proton.me>
2025-09-17 11:42:16 -0400

    working path spawn

--- scripts/EnemySpawner.cs
+++ scripts/EnemySpawner.cs
@@ -9,27 +9,38 @@ public partial class EnemySpawner : Node2D
     [Export]
     private int maxEnemies = 30;
 
+    [Export] private PackedScene pathEnemyScene;
+
     private Array<Node> positions;
     private int currentEnemy;
-    private Timer timer;
+    private Timer spawnNormalEnemiesTimer;
+    private Timer spawnPathEnemiesTimer;
 
     public override void _Ready()
     {
-        timer = GetNode<Timer>("Timer");
-        timer.Timeout += spawnEnemy;
+        spawnNormalEnemiesTimer = GetNode<Timer>("SpawnNormalEnemiesTimer");
+        spawnPathEnemiesTimer = GetNode<Timer>("SpawnPathEnemiesTimer");
+        spawnNormalEnemiesTimer.Timeout += SpawnEnemy;
+        spawnPathEnemiesTimer.Timeout += SpawnEnemyPath;
         positions = GetNode<Node2D>("Positions").GetChildren();
     }
 
-    private void spawnEnemy()
+    private void SpawnEnemy()
     {
         if (currentEnemy >= maxEnemies)
         {
-            timer._ExitTree();
+            spawnNormalEnemiesTimer._ExitTree();
             return;
         }
         var instance = enemyScene.Instantiate() as Enemy;
         AddChild(instance);
-        instance.Position = ((Marker2D)positions.PickRandom()).Position;
+        instance?.Position = ((Marker2D)positions.PickRandom()).Position;
         currentEnemy++;
+    }
+    
+    private void SpawnEnemyPath()
+    {
+        var instance = pathEnemyScene.Instantiate();
+        AddChild(instance);
     }
 }
